1f2d15aba116187f22b9e96d9b6ef9ad3042efd0
Ramiro <ramiro.depaoli@gmail.com>
2026-09-22 22:04:39 -0300

    ccache fix and remove patchs from building

--- src/net/minecraft/src/WorldRenderer.cpp
+++ src/net/minecraft/src/WorldRenderer.cpp
@@ -584,6 +584,7 @@ void WorldRenderer::markDirty()
 	needsUpdate = true;
 }
 
+#if WII_PLATFORM || PS2_PLATFORM || PLATFORM_PC_LEGACY
 void WorldRenderer::markDirtyFromLighting()
 {
 #if PLATFORM_COALESCE_MESH_REBUILDS
@@ -614,11 +615,14 @@ void WorldRenderer::markDirtyFromLighting()
 #endif
 	markDirty();
 }
+#endif
 
 void WorldRenderer::setDontDraw()
 {
+	#if WII_PLATFORM || PS2_PLATFORM || PLATFORM_PC_LEGACY
 	// Whatever edit marked this renderer urgent was at its old position.
 	urgentRebuild = false;
+	#endif
 #if PLATFORM_PC_LEGACY
 	pcLegacyResetBuildState();
 	for (int_t face = 0; face < 6; ++face)
